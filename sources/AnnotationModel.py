aa4e8ed96e73ba92aec1d6cbbfbc7ec5d32f7b32
hieunghia-pat <56166557+hieunghia-pat@users.noreply.github.com>
2023-04-22 22:02:22 +0700

    refined the loading annotations process

--- sources/AnnotationModel.py
+++ sources/AnnotationModel.py
@@ -77,18 +77,18 @@ class AnnotationModel(QAbstractListModel):
         
         return False
     
-    def insertRow(self, row: int, parent: Union[QModelIndex, QPersistentModelIndex] = ...) -> bool:
-        if row < 0 or row == len(self):
+    def insertRow(self, row: int, parent: QModelIndex=QModelIndex()) -> bool:
+        if row < 0 or row >= len(self):
             return False
         
         self.beginInsertRows(parent, row, row)
-        self.__data[row].insert(row, Annotation())
+        self.__data.insert(row, Annotation())
         self.endInsertRows()
 
         return True
 
-    def removeRow(self, row: int, parent: Union[QModelIndex, QPersistentModelIndex] = ...) -> bool:
-        if row < 0 or row == len(self):
+    def removeRow(self, row: int, parent: QModelIndex=QModelIndex()) -> bool:
+        if row < 0 or row >= len(self):
             return False
         
         self.beginRemoveRows(parent, row, row)
@@ -115,11 +115,11 @@ class AnnotationModel(QAbstractListModel):
         return [annotation.annotation for annotation in self.__data]
 
     @Property(int)
-    def index(self):
+    def selectedIndex(self):
         return self.__selectedIndex
     
-    @index.setter
-    def index(self, value: int):
+    @selectedIndex.setter
+    def selectedIndex(self, value: int):
         self.__selectedIndex = value
 
     @Slot(str)
@@ -129,34 +129,44 @@ class AnnotationModel(QAbstractListModel):
         index = self.createIndex(self.__selectedIndex, 0)
         self.setData(index, evidence, self.EVIDENCE)
     
-    @Slot(list)
-    def setAnnotations(self, annotations: List[Dict]):
-        assert len(annotations) > 0
+    @Slot(dict)
+    def setAnnotations(self, annotation: dict):
         # clear all previous annotations
         while self.rowCount() > 0:
-            self.removeRow(self.rowCount()-1)
+            self.removeRow(0)
 
         # reset the annotations
+        self.__context = annotation["context"]
         self.__data = []
-        for idx, annotation in enumerate(annotations):
+        if len(annotation["information"]) == 0:
+            self.__data = [{
+                "statement": "",
+                "verdict": 1,
+                "evidence": ""
+            }] * 10
+        else:
+            self.__data = annotation["information"]
+
+        for idx in range(len(self.__data)):
+            item = self.__data[idx]
             insertedRow = self.insertRow(idx)
             if not insertedRow:
-                self.rowInsertErrorSignal(f"Cannot insert row at index {idx}th")
+                self.rowInsertErrorSignal.emit(f"Cannot insert row at index {idx}th")
 
             setStatement = self.setData(self.index(idx, 0, QModelIndex()),
-                                      annotation["statement"], self.STATEMENT)
+                                      item["statement"], self.STATEMENT)
             if not setStatement:
-                self.setContextErrorSignal.emit(f"Cannot set statement for annotation {idx}th")
+                self.setStatementErrorSignal.emit(f"Cannot set statement for information {idx}th")
 
             setVerdict = self.setData(self.index(idx, 0, QModelIndex()),
-                                      annotation["verdict"], self.VERDICT)
+                                      item["verdict"], self.VERDICT)
             if not setVerdict:
-                self.setContextErrorSignal.emit(f"Cannot set verdict for annotation {idx}th")
+                self.setStatementErrorSignal.emit(f"Cannot set verdict for information {idx}th")
 
             setEvidence = self.setData(self.index(idx, 0, QModelIndex()),
-                                      annotation["evidence"], self.EVIDENCE)
+                                      item["evidence"], self.EVIDENCE)
             if not setEvidence:
-                self.setContextErrorSignal.emit(f"Cannot set evidence for annotation {idx}th")
+                self.setStatementErrorSignal.emit(f"Cannot set evidence for information {idx}th")
     
     @Slot(int)
     def addAnnotation(self, idx: int) -> bool:
